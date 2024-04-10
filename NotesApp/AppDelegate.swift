ebf52d76eb2abce4e99a7b59bb0d59005c0a61df
balandzin <balandzin2602@gmail.com>
2024-04-10 13:18:47 +0300

    add window

--- NotesApp/AppDelegate.swift
+++ NotesApp/AppDelegate.swift
@@ -10,10 +10,19 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        if let window {
+            
+            let navigationController = UINavigationController()
+            navigationController.viewControllers = [ViewController()]
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+        }
         return true
     }
-
 }
 
