0dc5d67c76c6a1086e794a25a238545f8b316609
balandzin <balandzin2602@gmail.com>
2024-04-10 13:13:39 +0300

    initial commit

---
+++ NotesApp/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  NotesApp
+//
+//  Created by Антон Баландин on 10.04.24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
